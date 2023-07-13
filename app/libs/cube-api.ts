f939d7d806290a6232fe9d58320bad0b870389fc
Luis Mori Guerra <luismorith@gmail.com>
2023-07-12 21:49:07 -0500

    feat: updating api
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- app/libs/cube-api.ts
+++ app/libs/cube-api.ts
@@ -2,7 +2,8 @@ import cubejs from "@cubejs-client/core";
 export const cubejsApi = cubejs(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODkwNTA0NjN9.z7MWOLezhBnT-iJuH09SsPje90TSP5K3EzIg10EQy-Q",
   {
-    apiUrl: "http://localhost:4000/cubejs-api/v1",
-    // "https://jade-trout.aws-us-west-2-t-11709.cubecloudapp.dev/cubejs-api/v1",
+    apiUrl:
+      "https://jade-trout.aws-us-west-2-t-11709.cubecloudapp.dev/cubejs-api/v1",
+    // "http://localhost:4000/cubejs-api/v1",
   }
 );
